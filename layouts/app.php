f5d69873fb67a5124a44ae6772413bb76b3e90e0
itsrafsanjani <mdrafsanjanirafin@gmail.com>
2021-08-08 00:10:57 +0600

    feat: favicon added

--- layouts/app.php
+++ layouts/app.php
@@ -2,13 +2,14 @@
 <html>
 
 <head>
+    <title>BUBT Result</title>
+    <link rel="icon" type="image/svg+xml" href="favicon.svg" />
+    <!--Let browser know website is optimized for mobile-->
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <!--Import Google Icon Font-->
     <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet" />
     <!-- Compiled and minified CSS -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css" />
-    <!--Let browser know website is optimized for mobile-->
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>BUBT Result</title>
 </head>
 
 <body>
